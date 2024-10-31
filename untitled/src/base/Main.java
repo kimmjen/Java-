c6800bc41d773fa83da960528507c186c02cd6bb
kimmjen <wpals814@gmail.com>
2024-10-31 15:01:23 +0900

    기본 세팅 및 문제 풀이

---
+++ untitled/src/base/Main.java
@@ -0,0 +1,16 @@
+package base;
+
+
+public class Main {
+
+    public int solution(String str, char t) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        System.out.println();
+    }
+}
